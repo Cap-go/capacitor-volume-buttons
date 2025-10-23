ff16145c8e5699f84bdf2e9ce7d2f879b9947878
Martin Donadieu <martindonadieu@gmail.com>
2025-10-23 13:40:23 +0100

    fix: add missing imports for PluginCall and PluginMethod

--- android/src/main/java/com/capgo/volumebuttons/VolumeButtonsPlugin.java
+++ android/src/main/java/com/capgo/volumebuttons/VolumeButtonsPlugin.java
@@ -4,6 +4,8 @@ import android.view.KeyEvent;
 import android.view.View;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "VolumeButtons")
