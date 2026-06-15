14ce6172af46342d282fd9b973124eb54575863c
Martin Donadieu <martindonadieu@gmail.com>
2026-06-15 23:50:55 +0200

    fix: address Capgo example deploy review

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -11,8 +11,10 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-        .package(name: "CapgoCapacitorVolumeButtons", path: "../../../node_modules/.bun/@capgo+capacitor-volume-buttons@file+../node_modules/@capgo/capacitor-volume-buttons")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
+        .package(name: "CapgoCapacitorVolumeButtons", path: "../../../node_modules/.bun/@capgo+capacitor-volume-buttons@file+../node_modules/@capgo/capacitor-volume-buttons"),
+        .package(name: "CapgoCapacitorUpdater", path: "../../../node_modules/.bun/@capgo+capacitor-updater@8.47.10+15e98482558ccfe6/node_modules/@capgo/capacitor-updater"),
+        .package(name: "CapacitorSplashScreen", path: "../../../node_modules/.bun/@capacitor+splash-screen@8.0.1+15e98482558ccfe6/node_modules/@capacitor/splash-screen")
     ],
     targets: [
         .target(
@@ -20,7 +22,9 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "CapgoCapacitorVolumeButtons", package: "CapgoCapacitorVolumeButtons")
+                .product(name: "CapgoCapacitorVolumeButtons", package: "CapgoCapacitorVolumeButtons"),
+                .product(name: "CapgoCapacitorUpdater", package: "CapgoCapacitorUpdater"),
+                .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen")
             ]
         )
     ]
